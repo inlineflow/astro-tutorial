78be617a296dd938ea490cccee9dc5f151ef78a7
inlineflow <thrashdev@protonmail.com>
2025-06-20 17:29:23 +0600

    added a lot of shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
+  site: "https://example.com",
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [preact()],
 });
